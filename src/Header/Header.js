2a6599b51a656b57dd1c1efca09343eeb8e9fe09
Anton Kuzmin, Moscow, +7(925)708-50-70 <antofer@mail.ru>
2019-07-07 16:35:42 +0200

    Header converted to fn

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,29 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import SearchField from "../SearchField/SearchField";
 
-export default class Header extends React.Component {
-    render(){
-        return (
-            <header>
-                <SearchField/>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Каталог</Link>
-                        </li>
-                        <li>
-                            <Link to="/have-to-buy/">Что нужно купить</Link>
-                        </li>
-                        <li>
-                            <Link to="/sources/">Магазины</Link>
-                        </li>
-                        <li>
-                            <Link to="/publishers/">Издательства</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        );
-    }
+export default function Header() {
+
+    return (
+        <header>
+            <SearchField/>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Каталог</Link>
+                    </li>
+                    <li>
+                        <Link to="/have-to-buy/">Что нужно купить</Link>
+                    </li>
+                    <li>
+                        <Link to="/sources/">Магазины</Link>
+                    </li>
+                    <li>
+                        <Link to="/publishers/">Издательства</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
