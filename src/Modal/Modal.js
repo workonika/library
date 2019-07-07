879037153b9ac1ec853b09ead0abdd44019af641
Anton Kuzmin, Moscow, +7(925)708-50-70 <antofer@mail.ru>
2019-07-07 23:01:18 +0200

    Из компонента Modal удалён не работающий код

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -5,25 +5,12 @@ export default function Modal (){
 
     let cssClassModal = "fix substrate";
 
-    function addREmoveClass(cssClass, str){
-
-        return function(){
-            let strMap = str.split(/\s/);
-            if(strMap.indexOf(cssClass) === -1){
-                strMap.push(cssClass);
-                return strMap.join(" ");
-            } else {
-                return strMap.filter(str=>str != cssClass);
-            }
-        }
-    }
-
     return (
         <div className={cssClassModal}>
             <div className={"fix modal border background shadow"}>
                 <div className={"header"}>
                     <h3>Заголовок модального окна</h3>
-                    <span className={"close"} title={"Закрыть"} onClick={addREmoveClass("hide", cssClassModal)}>закрыть</span>
+                    <span className={"close"} title={"Закрыть"}>закрыть</span>
                 </div>
                 Модальное окно
             </div>
