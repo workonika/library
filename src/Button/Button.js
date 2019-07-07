d70336fd103e6ab81bfb799c187d97cc9df733ac
Anton Kuzmin, Moscow, +7(925)708-50-70 <antofer@mail.ru>
2019-07-07 22:17:44 +0200

    Добавлен компонент Button

---
+++ src/Button/Button.js
@@ -0,0 +1,6 @@
+import React, {useState, useEffect} from "react";
+
+export default function Button(props){
+    const buttonText = props.buttonText ? props.buttonText : "Кнопка";
+    return <button onClick={props.onClick}>{buttonText}</button>
+}
