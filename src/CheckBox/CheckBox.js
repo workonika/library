b3a77d437840bd2d531d0e6a7e884675efb9a73e
Anton Kuzmin, Moscow, +7(925)708-50-70 <antofer@mail.ru>
2019-07-09 21:01:52 +0200

    Изменён чекбокс и удален код который обрабатывал уже встроенную функциональность@

--- src/CheckBox/CheckBox.js
+++ src/CheckBox/CheckBox.js
@@ -2,23 +2,24 @@ import React, {useState, useEffect} from "react";
 
 export default function CheckBox (props) {
     
-    const id = Math.round(Math.random() *(new Date).getTime());
-    const [checkboxState, changeCheckboxState] = useState({checked: false});
+    const id = props.id || Math.round(Math.random() *(new Date).getTime());
+    const [checked, setChecked] = useState(false);
     const defaultText = "Сменить чекбокс";
+    const labelText = props.labelText ? props.labelText: defaultText;
 
     const onCheckBoxChange = changeState;
     
     function changeState(){
-        changeCheckboxState({checked: !checkboxState.checked});
+        setChecked(!checked);
     }
 
     useEffect(()=>{
         const checkbox = document.getElementById(id);
-        checkbox.checked = checkboxState.checked;
+        checkbox.checked = checked;
     });
 
     return <React.Fragment>
-            <label htmlFor={id}>{props.labelText ? props.labelText: defaultText}</label>
-            <input id={id} type="checkbox" defaultChecked={checkboxState.checked} onChange={onCheckBoxChange}/>
+            <label htmlFor={id}>{labelText}</label>
+            <input id={id} type="checkbox" defaultChecked={checked} onChange={onCheckBoxChange}/>
         </React.Fragment>
 }
