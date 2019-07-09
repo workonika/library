d44b148e929bc2885528ca121a9af7c6b7bbf0ba
Anton Kuzmin, Moscow, +7(925)708-50-70 <antofer@mail.ru>
2019-07-09 23:24:45 +0200

    В рендер бокс добавлена обработка пропс и передача пропс компоненту SelectCheckboxes

--- src/RenderBox/RenderBox.js
+++ src/RenderBox/RenderBox.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import SelectCheckboxes from "../SelectCheckboxes/SelectCheckboxes";
 
 export default function RenderBox (props) {
-    
-console.log("props:", props);
+  
+    const [state, setState] = useState(true);
 
-return (<ul>
-    <li>Demo</li>
-</ul>);
+    const [books, setBooks] = useState(props.books);
+
+    function changeStateSelectCheckboxes (state){
+        setState(state);
+    }
+
+    return <React.Fragmet>
+        <div className={"empty"}>
+            <SelectCheckboxes condition={"selected-partially"} setState={changeStateSelectCheckboxes} />
+        </div>
+        <ul>
+            {books.map(book=><li><input type="checkbox" id={book}></input>{book}</li>)}
+        </ul>
+    </React.Fragmet>
 }
