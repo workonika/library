f6c9b806c21d8ae895b8f15da2376fa79a535932
Anton Kuzmin, Moscow, +7(925)708-50-70 <antofer@mail.ru>
2019-07-13 17:21:16 +0200

    Добавлены и изменены стили

--- src/ControlPanel/ControlPanel.js
+++ src/ControlPanel/ControlPanel.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import SelectCheckboxes from "../SelectCheckboxes/SelectCheckboxes";
 import Button from "../Button/Button";
+import "./ControlPanel.css";
 
 export default function ControlPanel (props){
     const {condition, parentCallback} = props.selectCheckboxesProps;
     const {buttonText} = props.buttonProps;
     
-    return <div>
+    return <div className={"control-panel"}>
         <SelectCheckboxes condition={condition} parentCallback={parentCallback}/>
         <Button buttonText={buttonText}/>
         </div>;
